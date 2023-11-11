2325c9e1f10d37667ce3a1403bd37d2f17e91439
reddevilmidzy <midzy0228@gmail.com>
2023-11-12 02:27:18 +0900

    test: 수량이 숫자가 아닌 경우 예외

--- src/test/java/christmas/model/OrderTest.java
+++ src/test/java/christmas/model/OrderTest.java
@@ -24,6 +24,16 @@ class OrderTest {
     @ValueSource(strings = {"아이스크림-0", "양송이수프-0"})
     void createInvalidQuantity(String value) {
         assertThatThrownBy(() -> Order.from(value))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 유효하지 않은 주문입니다.");
+    }
+
+    @DisplayName("수량이 유효한 숫자가 아니면 예외")
+    @ParameterizedTest
+    @ValueSource(strings = {"크리스마스파스타-a", "크리스마스파스타-크리스마스파스타", "크리스마스파스타-."})
+    void createInvalidType(String value) {
+        assertThatThrownBy(() -> Order.from(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 유효하지 않은 주문입니다.");
     }
 }
