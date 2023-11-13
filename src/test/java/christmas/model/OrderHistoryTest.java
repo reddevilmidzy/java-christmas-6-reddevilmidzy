ec8db3d5b5227abb85b5556bb782f6506d4964ba
reddevilmidzy <midzy0228@gmail.com>
2023-11-13 15:31:59 +0900

    test: 잘못된 구분자 형식인 경우 예외 테스트 생성

--- src/test/java/christmas/model/OrderHistoryTest.java
+++ src/test/java/christmas/model/OrderHistoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.stream.Stream;
@@ -76,9 +77,19 @@ class OrderHistoryTest {
 
     static Stream<Arguments> onlyBeverageOrderParametersProvider() {
         return Stream.of(
-                Arguments.of("제로콜라-1", "레드와인-1"),
+                Arguments.of("제로콜라-1,레드와인-1"),
                 Arguments.of("샴페인-3"),
-                Arguments.of("제로콜라-10", "레드와인-2", "샴페인-4")
+                Arguments.of("제로콜라-10,레드와인-2,샴페인-4")
         );
+    }
+
+    @DisplayName("잘못된 형식의 구분자 입력시 예외 발생")
+    @ParameterizedTest(name = "{displayName}: {0}")
+    @ValueSource(strings = {",,", ",해산물파스타-1", "시저샐러드-1,,레드와인-1", "아이스크림-1제로콜라-1"})
+    @NullSource
+    void createInvalidSeparator(String value) {
+        assertThatThrownBy(() ->
+                OrderHistory.from(value))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
